a46767d95549ee85860803c98d4e7bdaab173867
Fernanda dos Santos <159660295+fe-tabs@users.noreply.github.com>
2024-06-02 22:36:34 -0300

    :sparkles: save form data in localStorage

--- js/script.js
+++ js/script.js
@@ -2,7 +2,24 @@ import isOfAge from "./validate-birth-date.js";
 import isValidCPF from "./validate-cpf.js";
 import { errorTypes, errorMessages } from "./errors.js";
 
+const form = document.querySelector("[data-formulario]");
 const formFields = document.querySelectorAll("[required]");
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const data = {
+    "name": event.target.element["nome"].value,
+    "email": event.target.element["email"].value,
+    "rg": event.target.element["rg"].value,
+    "cpf": event.target.element["cpf"].value,
+    "birthDate": event.target.element["aniversario"].value
+  };
+
+  localStorage.setItem("register", JSON.stringify(data));
+
+  window.location.href = './abrir-conta-form-2.html';
+})
 
 formFields.forEach(field => {
   field.addEventListener("blur", () => verifyField(field));
